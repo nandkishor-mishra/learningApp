17399d1d76c80f7f5f10c272c45831b2351a0442
Nandkishor Mishra <nandkishor_mishra@epam.com>
2025-09-18 14:58:36 +0530

    Updated Code

--- learningapp/Pages/Index.cshtml.cs
+++ learningapp/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
 
 namespace learningapp.Pages;
 
@@ -15,23 +16,38 @@ public class IndexModel : PageModel
         _configuration=configuration;
     }
 
-    public void OnGet()
-    {
+    //public void OnGet()
+    //{
        
-        string connectionString = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")!;
-        var sqlConnection = new SqlConnection(connectionString);
-        sqlConnection.Open();
+    //    string connectionString = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")!;
+    //    var sqlConnection = new SqlConnection(connectionString);
+    //    sqlConnection.Open();
 
-        var sqlcommand = new SqlCommand(
-        "SELECT CourseID,CourseName,Rating FROM Course;",sqlConnection);
-         using (SqlDataReader sqlDatareader = sqlcommand.ExecuteReader())
-         {
-             while (sqlDatareader.Read())
-                {
-                    Courses.Add(new Course() {CourseID=Int32.Parse(sqlDatareader["CourseID"].ToString()),
-                    CourseName=sqlDatareader["CourseName"].ToString(),
-                    Rating=Decimal.Parse(sqlDatareader["Rating"].ToString())});
-                }
-         }
+    //    var sqlcommand = new SqlCommand(
+    //    "SELECT CourseID,CourseName,Rating FROM Course;",sqlConnection);
+    //     using (SqlDataReader sqlDatareader = sqlcommand.ExecuteReader())
+    //     {
+    //         while (sqlDatareader.Read())
+    //            {
+    //                Courses.Add(new Course() {CourseID=Int32.Parse(sqlDatareader["CourseID"].ToString()),
+    //                CourseName=sqlDatareader["CourseName"].ToString(),
+    //                Rating=Decimal.Parse(sqlDatareader["Rating"].ToString())});
+    //            }
+    //     }
+    //}
+    public async Task<IActionResult> OnGet()
+    {
+        string? funcURL = Environment.GetEnvironmentVariable("CourseFunction");
+        //string? funcURL = "https://funcapp0311-gaa9hfcqg4fmfcet.canadacentral-01.azurewebsites.net/api/appFunction";
+        using (var httpClient = new HttpClient())
+        {
+            var response = await httpClient.GetAsync(funcURL);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                Courses = JsonConvert.DeserializeObject<List<Course>>(jsonResponse);
+            }
+        }
+        return Page();
     }
 }
